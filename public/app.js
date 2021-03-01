094f462193d8d5bd425f2a900e79400b5619f730
Keith Van Rhein <kvanrhein@cps.edu>
2021-03-01 08:32:58 -0600

    to bare bones

--- public/app.js
+++ public/app.js
@@ -1,22 +1,13 @@
 var stripe1black = false;
 var socket = io();
-socket.emit("stripe2")
 
 socket.on("paintblack", () => {
   stripe1black = !stripe1black;
-  document.getElementById("stripe1").style.backgroundColor =  stripe1black ? "#000000" : "#ff0000";
+  document.getElementById("stripe").style.backgroundColor =  stripe1black ? "#000000" : "#ff0000";
 })
 
-function stripe2clicked() {
-  stripe1black = !stripe1black;
- // document.getElementById("stripe1").style.backgroundColor = stripe1black ? "#000000" : "#ff0000";
-  socket.emit("stripe2")
-}
-
-function stripe3clicked() {
-  document.getElementById("stripe1").style.backgroundColor = "#000044";
-}
-
 function stripe1clicked() {
-  document.getElementById("stripe1").style.backgroundColor = "#000044";
+  stripe1black = !stripe1black;
+  socket.emit("stripe")
+  console.log("clicked stripe")
 }
--- server.js
+++ server.js
@@ -9,87 +9,22 @@ const io = socketio(server)
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")))
-
-// Start server
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-
-// Handle a socket connection request from web client
-const connections = [null, null]
-
 io.on('connection', socket => {
-  let playerIndex = -1;
-  for (const i in connections) {
-    if (connections[i] === null) {
-      playerIndex = i
-      break
-    }
-  }
+  console.log("Someone joined")
+  socket.broadcast.emit('player-connection')
 
-  // Tell the connecting client what player number they are
-  socket.emit('player-number', playerIndex)
-
-  console.log(`Player ${playerIndex} hasdfs connected`)
-
-  // Ignore player 3
-  if (playerIndex === -1) return
-
-  connections[playerIndex] = false
-
-  // Tell eveyone what player number just connected
-  socket.broadcast.emit('player-connection', playerIndex)
-
-  // Handle Diconnect
   socket.on('disconnect', () => {
-    console.log(`Player ${playerIndex} disconnected`)
-    connections[playerIndex] = null
-    //Tell everyone what player numbe just disconnected
-    socket.broadcast.emit('player-connection', playerIndex)
+    console.log('Someone left')
   })
 
-  // On Ready
-  socket.on('player-ready', () => {
-    socket.broadcast.emit('enemy-ready', playerIndex)
-    connections[playerIndex] = true
-  })
-
-  // On Ready
-  socket.on('stripe2', id => {
-    console.log("clicked 2")
+  socket.on('stripe', () => {
+    console.log("clicked")
     socket.broadcast.emit("paintblack")
   })
-  // Check player connections
-  socket.on('check-players', () => {
-    const players = []
-    for (const i in connections) {
-      connections[i] === null ? players.push({connected: false, ready: false}) : players.push({connected: true, ready: connections[i]})
-    }
-    socket.emit('check-players', players)
-  })
 
-  // On Fire Received
-  socket.on("hello", () => {
-        console.log("hello")
-  })
-
-  socket.on('fire', id => {
-    console.log(`Shot fired from ${playerIndex}`, id)
-
-    // Emit the move to the other player
-    socket.broadcast.emit('fire', id)
-  })
-
-  // on Fire Reply
-  socket.on('fire-reply', square => {
-    console.log(square)
-
-    // Forward the reply to the other player
-    socket.broadcast.emit('fire-reply', square)
-  })
-
-  // Timeout connection
   setTimeout(() => {
-    connections[playerIndex] = null
     socket.emit('timeout')
     socket.disconnect()
-  }, 600000) // 10 minute limit per player
+  }, 600000) // 10 minute limit
 })
